32842744b8b5f10d08879da69aeb6783c6387c8e
karanj7890 <124456783+karanj7890@users.noreply.github.com>
2023-11-24 11:10:11 +0530

    image project

---
+++ app.js
@@ -0,0 +1,59 @@
+const accessKey= "BukNB42TicnfHaEQGEqMZ-ovoeqgqqk_oPvmeo5pIAs"
+
+const formEl= document.querySelector("form")
+const inputEl= document.getElementById("Search")
+const searchResults= document.querySelector(".search-results")
+const show= document.getElementById("show-more-button")
+
+let inputdata= ''
+let page= 1;
+
+async function searchImages(){
+    inputdata= inputEl.value 
+    const url= `https://api.unsplash.com/search/photos?page=${page}&query=${inputdata}&client_id=${accessKey}`
+
+    const response= await fetch(url)
+    const data=  await response.json()
+
+    const results= data.results
+
+    if(page === 1){
+        searchResults.innerHTML= ""
+    }
+
+    results.map((result)=>{
+        const imageWrapper= document.createElement('div')
+        imageWrapper.classList.add("search-result")
+        const image= document.createElement('img')
+        image.src= result.urls.small
+        image.alt= result.alt_description
+        const imageLink= document.createElement('a')
+        imageLink.href= result.links.html
+        imageLink.target= "_blank"
+        imageLink.textContent= result.alt_description
+
+        imageWrapper.appendChild(image)
+        imageWrapper.appendChild(imageLink)
+        searchResults.appendChild(imageWrapper)
+    });
+
+
+    page++
+    if(page > 1){
+        show.style.display= "block"
+
+    }
+
+}
+
+formEl.addEventListener("submit", (event)=>{
+    event.preventDefault()
+    page=1
+    searchImages()
+
+})
+
+show.addEventListener("click", ()=>{
+    searchImages()
+    
+})
